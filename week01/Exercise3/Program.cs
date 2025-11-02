6ce2dd51154df28ad471d25fc1ff40924cc44c12
juliocesar1113 <juliospm3728@gmail.com>
2025-11-01 23:21:54 -0600

    3rd exercise

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.Write("What is the magic number? ");
+        string magicInput = Console.ReadLine();
+        int magic;
+        if (!int.TryParse(magicInput, out magic))
+        {
+            Console.WriteLine("Invalid number.");
+            return;
+        }
+
+        Console.Write("What is your guess? ");
+        string guessInput = Console.ReadLine();
+        int guess;
+        if (!int.TryParse(guessInput, out guess))
+        {
+            Console.WriteLine("Invalid guess.");
+            return;
+        }
+
+        if (guess < magic)
+            Console.WriteLine("Higher");
+        else if (guess > magic)
+            Console.WriteLine("Lower");
+        else
+            Console.WriteLine("You guessed it!"); 
+        
     }
-}
+    }
